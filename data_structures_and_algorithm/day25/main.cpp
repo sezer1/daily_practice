491972dbf2cdb4c8a2aeebe200a5fa68541d9226
= <=>
2023-06-30 23:24:41 +0300

    query

---
+++ data_structures_and_algorithm/day25/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+//bu yapıları doldur
+
+
+struct Kisi {
+char isim[10];
+
+};
+
+int kapasite=10;
+
+struct Kisi[10];
+int SP;
+int DoluMu(){
+ if(SP<kapasite)
+    return 1;
+ else
+   return 0;
+
+
+};
+
+void Push();
+void Pull();
+
+
+
+int main()
+{
+
+
+
+
+    return 0;
+}
