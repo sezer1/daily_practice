f17172a4605f377987ae57e1eb20ca7c6e42e4a9
= <=>
2023-07-30 14:39:35 +0300

    recursive binary search tree

--- data_structures_and_algorithm/day38/main.cpp
+++ data_structures_and_algorithm/day38/main.cpp
@@ -9,7 +9,6 @@ int  pivot_selection(int *A, int ilk, int son){
     int v=A[ilk];
     int tmp;
     while(1){
-
         while(A[i]<=v && i<=son)
             i++;
         while(A[j]>v)
@@ -27,31 +26,30 @@ int  pivot_selection(int *A, int ilk, int son){
     return j;
 }
 
-void quick_sort(int *A,int ilk, int son){
-    if(son<=ilk)
-      return;
+void quick_sort(int *A,int ilk, int son)
+{
+  if(son<=ilk)
+  return;
   int j=pivot_selection(A,ilk,son);
-
   quick_sort(A,ilk,j-1);
   quick_sort(A,j+1,son);
-
-
 }
 
 int main()
 {
- int A[]={10,4,9,6,2,15,12,17,13};
 
+ int A[]={10,4,9,6,2,15,12,17,13};
  printf("sirasiz: ");
-for(int i=0;i<8;i++){
+ for(int i=0;i<8;i++){
     printf("%d ",A[i]);
-}
+ }
+
  quick_sort(A,0,7);
 
  printf("\nsiralanmis: ");
-for(int i=0;i<8;i++){
+ for(int i=0;i<8;i++){
     printf("%d ",A[i]);
-}
+ }
  printf("\n");
 
  return 0;
---
+++ data_structures_and_algorithm/day39/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+struct AGACDUGUMPTR{
+int data;
+struct AGACDUGUMPTR *sol;
+struct AGACDUGUMPTR *sag;
+};
+
+
+AGACDUGUMPTR *dugumekle(AGACDUGUMPTR *agacptr,int veri){
+ if(agacptr==NULL){
+    agacptr=(AGACDUGUMPTR *)malloc(sizeof(AGACDUGUMPTR));
+        if(agacptr!=NULL){
+            agacptr->data=veri;
+            agacptr->sol=NULL;
+            agacptr->sag=NULL;
+        }
+        else{printf("%d eklenemedi",veri);}
+ }
+ else{
+    if(veri<agacptr->data){
+        printf("agacın soluna veri eklendi\n");
+        agacptr->sol=dugumekle(agacptr->sol,veri);
+    }else{
+    if(veri>agacptr->data){
+        printf("agacın sag tarafına veri eklendi");
+        agacptr->sag=dugumekle(agacptr->sag,veri);
+    }else printf("eşit olduğu için eklenemedi\n");
+    }
+ }
+
+return agacptr;
+}
+
+void inorder(AGACDUGUMPTR *agacptr){
+    if(agacptr !=NULL){
+        inorder(agacptr->sol);
+        printf("%3d",agacptr->data);
+        inorder(agacptr->sag);
+
+    }
+}
+
+
+
+int main()
+{
+    struct AGACDUGUMPTR *p= (AGACDUGUMPTR *)malloc(sizeof(AGACDUGUMPTR));
+    p=dugumekle(p,15);
+    p=dugumekle(p,3);
+    p=dugumekle(p,12);
+    p=dugumekle(p,7);
+    p=dugumekle(p,9);
+    p=dugumekle(p,42);
+    p=dugumekle(p,11);
+    inorder(p);
+
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
